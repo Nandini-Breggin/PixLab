a587040d13957d3c5b0bd24dc68952596dbb5703
Nandini Breggin <bregginnr@s.dcsdk12.org>
2021-01-16 17:04:54 -0700

    Add Activity3

---
+++ answers.md
@@ -0,0 +1,7 @@
+* 0
+* 0
+* 639
+* 479
+* top to bottom -> row index increases
+* left to right -> column index increases
+* Yes. I can see the squares of color (pixels)
--- src/PictureExplorer.java
+++ src/PictureExplorer.java
@@ -796,8 +796,13 @@ public class PictureExplorer implements MouseMotionListener, ActionListener, Mou
 	 * Test Main. It will explore the beach
 	 */
 	public static void main(String args[]) {
-		Picture pix = new Picture("beach.jpg");
-		pix.explore();
+		// Picture pix = new Picture("beach.jpg");
+		// pix.explore();
+
+		Picture p = new Picture("champ.jpg");
+		Picture pix2 = p.scale(10, 10);
+		pix2.write("smallChamp.jpg");
+		pix2.explore();
 	}
 
 }
